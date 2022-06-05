e336fdd8669388902ce5c29606df8f9ebe291f87
Azadul Islam Rasel <azad.is.rasel@gmail.com>
2022-06-05 11:27:19 +0600

    component created and tailwind setup

---
+++ src/Pages/Channel/Channel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Channel = () => {
+    return (
+        <div>
+            <h2>This is Channel</h2>
+        </div>
+    );
+};
+
+export default Channel;
---
+++ src/Pages/UploadPage/UploadPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UploadPage = () => {
+    return (
+        <div>
+            <h2>This is Upload page</h2>
+        </div>
+    );
+};
+
+export default UploadPage;
